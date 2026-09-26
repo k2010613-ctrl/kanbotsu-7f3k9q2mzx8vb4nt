aa591500b6d7d203ab6cbef8a13f466f4a40560d
k2010613-ctrl <k2010613@gmail.com>
2026-09-26 18:17:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kanbotsu-v8';
+const CACHE = 'kanbotsu-v9';
 const FILES = ['./', './index.html', './lite.html', './full.html', './explain.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
